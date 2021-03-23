c32b86b8046362a1eff2b4da6652aa502e9053aa
Solda <solda@khu.ac.kr>
2021-03-23 16:21:49 +0900

    updated

---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// 비어 있는 업로드 페이지 생성
+
+function UploadProductPage() {
+    return (
+        <div>
+            안녕하세요
+        </div>
+    )
+}
+
+export default UploadProductPage
